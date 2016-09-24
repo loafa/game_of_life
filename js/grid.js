d72dd88290ddc712b0257300ee524f91951008dc
Sophie Russo <ssrusso@mit.edu>
2016-09-24 14:37:14 -0400

    added tests for grid and game

--- js/grid.js
+++ js/grid.js
@@ -1,17 +1,19 @@
 // creates a square grid for gameplay
 // square_dim: dimension of each cell square (in pixels)
-// squares_per_row: number of squares in each row/ column
+// board_dim: number of squares in each row/ column
 // canvas: the HTML5 canvas object to create the grid in
 //			(if not passed an object, will attach a canvas element named 'grid' to the doc)
-var Grid = function(square_dim, squares_per_row, canvas) {
+var Grid = function(square_dim, board_dim, canvas) {
 	var that = Object.create(Grid.prototype);
-
+	var DEFAULT_SQUARE_DIM = 20;
+	var DEFAULT_BOARD_DIM = 30;
+	
 	// defaults. if not specified, we say that:
 	// a square will be 20 x 20px
 	// there will be 30 squares in every row and column
 	// we create a canvas element called "grid", which will be used to draw all the game elements
-	if (square_dim == undefined) { square_dim = 20; }
-	if (squares_per_row == undefined) { squares_per_row = 30; }
+	if (square_dim == undefined) { square_dim = DEFAULT_SQUARE_DIM; }
+	if (board_dim == undefined) { board_dim = DEFAULT_BOARD_DIM; }
 	if (canvas == undefined) {
 		if (document.getElementById("grid") == undefined){
 			$('<canvas>').attr({ id: "grid" }).appendTo('body');
@@ -20,7 +22,7 @@ var Grid = function(square_dim, squares_per_row, canvas) {
 	}
 
 	// the dimension of the square in pixels
-	var pixel_dim = square_dim * squares_per_row;
+	var pixel_dim = square_dim * board_dim;
 
 	// update the canvas to the appropriate dimensions to have a nice square grid
 	canvas.setAttribute("width", pixel_dim);
@@ -54,9 +56,9 @@ var Grid = function(square_dim, squares_per_row, canvas) {
 		that.drawGridLines();
 	};
 
-	// draws the grid lines according to the parameters specified above (eg. squares_per_row)
+	// draws the grid lines according to the parameters specified above (eg. board_dim)
 	that.drawGridLines = function() {
-		for (var i = 0; i <= squares_per_row; i++) {
+		for (var i = 0; i <= board_dim; i++) {
 			// draw horizontal lines
 			ctx.moveTo(0, square_dim * i);
 		    ctx.lineTo(pixel_dim, square_dim * i);
--- tests.js
+++ tests.js
@@ -176,6 +176,144 @@
             });
         });
     });
+    
+    describe("Grid", function() {
+
+        describe("dimension", function() {
+            it("should return the correct pixel dimension of the default grid", function(){
+                var grid = Grid();
+                var default_square_dim = 20; //pixels
+                var default_board_dim = 30; //squares
+                assert.equal(default_square_dim*default_board_dim, grid.dimension());
+            });
+            it("should return correct pixel dimension of custom grid", function() {
+                var square_dim = 10; //pixels
+                var board_dim = 40; //squares
+                var grid = Grid(square_dim, board_dim);
+                assert.equal(square_dim * board_dim, grid.dimension());
+            });
+        });
+
+        describe("convertToCoord", function() {
+            it("should return the correct board coordinates for point in default grid", function(){
+                var grid = Grid();
+                var default_square_dim = 20; //pixels
+                // 34 in x -> 34/square_dim -> 1 (int division)
+                // 70 in y -> 70/square_dim -> 3
+                assert.equal(1, grid.convertToCoord(34, 70).x);
+                assert.equal(3, grid.convertToCoord(34, 70).y);
+            });
+            it("should return the correct board coordinates for point in custom grid", function(){
+                var square_dim = 10; //pixels
+                var grid = Grid(square_dim, 42);
+                // 34 in x -> 34/square_dim -> 3
+                // 70 in y -> 70/square_dim -> 7
+                assert.equal(3, grid.convertToCoord(34, 70).x);
+                assert.equal(7, grid.convertToCoord(34, 70).y);
+            });
+        });
+        
+    });
+
+    // we won't test graphical behavior, but can verify that the game logic behaves as we expect
+    // tests are more minimal, as most behavior has been tested by Board
+    describe("Game", function() {
+
+        // note this also tests game.start() and game.stop()
+        describe("isRunning", function() {
+            it("should return true when game is running", function(){
+                var game = Game();
+                game.start();
+                assert.isTrue(game.isRunning());
+                game.stop();
+                $("#grid").remove();
+            });
+            it("should return false when game is not running", function() {
+                var game = Game();
+                assert.isFalse(game.isRunning());
+                $("#grid").remove();
+            });
+        });
+
+        describe("toggleCell", function() {
+            it("should turn the top left square alive", function(){
+                var board = Board();
+                var grid = Grid();
+                var game = Game(grid, board);
+                game.toggleCell(1, 2);
+                assert.isTrue(board.isAlive(0, 0));
+                game.stop();
+                $("#grid").remove();
+            });
+
+            it("should turn the top left square dead", function(){
+                var board = Board();
+                var grid = Grid();
+                var game = Game(grid, board);
+                game.toggleCell(1, 2);
+                game.toggleCell(1, 2);
+                assert.isFalse(board.isAlive(0, 0));
+                game.stop();
+                $("#grid").remove();
+            });
+        });
+
+        describe("listInit", function() {
+            it("should initialize game from list of live cells", function(){
+                var board = Board();
+                var grid = Grid();
+                var game = Game(grid, board);
+                game.listInit([[1, 2], [2, 2]]);
+                assert.isTrue(board.isAlive(1, 2));
+                assert.isTrue(board.isAlive(2, 2));
+                game.stop();
+                $("#grid").remove();
+            });
+        });
+
+        describe("update", function() {
+            it("should appropriately update board based on underpopulation", function(){
+                var board = Board();
+                var grid = Grid();
+                var game = Game(grid, board);
+                game.listInit([[1, 2], [2, 2]]);
+                game.update();
+                assert.isFalse(board.isAlive(1, 2));
+                assert.isFalse(board.isAlive(2, 2));
+                game.stop();
+                $("#grid").remove();
+            });
+
+            it("should appropriately update board based on stable population & rejuvenation", function(){
+                var board = Board();
+                var grid = Grid();
+                var game = Game(grid, board);
+                game.listInit([[1, 2], [2, 2], [2, 1]]);
+                assert.isFalse(board.isAlive(1, 1)); // should be rejuvenated in next round
+                game.update();
+                assert.isTrue(board.isAlive(1, 2));
+                assert.isTrue(board.isAlive(2, 2));
+                assert.isTrue(board.isAlive(2, 1));
+                assert.isTrue(board.isAlive(1, 1));
+                game.stop();
+                $("#grid").remove();
+            });
+            it("should appropriately update board based on overpopulation", function(){
+                var board = Board(3, 3);
+                var grid = Grid();
+                var game = Game(grid, board);
+                game.listInit([[1, 0], [2, 0], [1, 1], [2, 2], [2, 1]]);
+                game.update();
+                assert.isFalse(board.isAlive(1, 0));
+                assert.isFalse(board.isAlive(2, 0));
+                assert.isFalse(board.isAlive(1, 1));
+                assert.isFalse(board.isAlive(2, 1));
+                assert.isFalse(board.isAlive(2, 2));
+                game.stop();
+                $("#grid").remove();
+            });
+        });
+    });
 
     mocha.run();
 })()
