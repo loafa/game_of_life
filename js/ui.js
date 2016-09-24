6db0fd1b121994563186aee2104d45fa1652f0f3
Sophie Russo <ssrusso@mit.edu>
2016-09-23 23:49:27 -0400

    fixed bug with preset configurations

--- js/ui.js
+++ js/ui.js
@@ -38,10 +38,13 @@ var UI = function(game) {
 	$("#toggle").click(that.startButtonEvent);
 	$("#random").click(that.randomInit);
 	$("#empty").click(that.emptyInit);
-	$("#grid").click(function(evt) { 
-		game.toggleCell(evt.offsetX, evt.offsetY); 
+	$("#grid").click(function(evt) {
+		if (!game.isRunning()){
+			game.toggleCell(evt.offsetX, evt.offsetY); 
+		} 
 	});
 	$("#pulsar").click(function() {
+		that.stop();
 		game.listInit([[8, 11], [8, 17], [9, 11], [9, 17], [10, 11], [10, 12], [10, 16], [10, 17], 
 			[12, 7], [12, 8], [12, 9], [12, 12], [12, 13], [12, 15], [12, 16], [12, 19], [12, 20], 
 			[12, 21], [13, 9], [13, 11], [13, 13], [13, 15], [13, 17], [13, 19], [14, 11], [14, 12], 
@@ -52,6 +55,7 @@ var UI = function(game) {
 	});
 
 	$("#pentadecathlon").click(function() {
+		that.stop();
 		game.listInit([[13, 12], [13, 13], [13, 14], [13, 15], [13, 16], [13, 17], [14, 11], [14, 18], [15, 10], [15, 19], [16, 11], [16, 18], [17, 12], [17, 13], [17, 14], [17, 15], [17, 16], [17, 17]]);
 	});
 	Object.freeze(that);
