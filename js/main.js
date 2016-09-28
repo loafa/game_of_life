7efd9595d919db298bb22de8e4af7d1b33d57aa9
Sophie Russo <ssrusso@mit.edu>
2016-09-28 10:52:19 -0400

    removed google docs comment

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-// https://docs.google.com/forms/d/e/1FAIpQLSeH-5rZCFSTk5BI0DNTsf1553oPd7dVGSF_rp9I9zjEiVNTdQ/viewform?edit2=2_ABaOnucDcGL9U9DcjTadHVkA0ke5AnSzGs-LUDg3KGfjRSRfvUnm231c8Wd7pCA
 (function() {
     document.addEventListener('DOMContentLoaded', function () {
     	// create default UI and we're good to go
